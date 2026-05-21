d4c212f1c338a5b6ef8555959c2d418bc13c5c03
TwoRelix <bohoyodani@gmail.com>
2026-05-21 11:21:51 +0200

    estetica add-beer

--- frontend/cerveceria-app/src/app/components/add-beer/add-beer.ts
+++ frontend/cerveceria-app/src/app/components/add-beer/add-beer.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-beer',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-beer.html',
   styleUrl: './add-beer.css',
 })
-export class AddBeer {}
+export class AddBeer {
+  private fb = inject(FormBuilder);
+
+  beerForm = this.fb.group({
+    name: ['', Validators.required],
+    brand: ['', Validators.required],
+    type: ['', Validators.required],
+    alcohol: [0, [Validators.required, Validators.min(0.1)]],
+    description: ['']
+  });
+
+  onSubmit() {
+
+    if (this.beerForm.invalid) {
+      this.beerForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.beerForm.value);
+
+    this.beerForm.reset();
+  }
+}
