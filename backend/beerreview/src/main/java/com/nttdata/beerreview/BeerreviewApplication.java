f2f7f949e9105cf78ef8f76e67022cc46f2a2d27
Gabriel Picazo <gabrielpicazo@MacBook-Air-de-Gabriel.local>
2026-05-21 11:40:49 +0200

    version1

---
+++ backend/beerreview/src/main/java/com/nttdata/beerreview/BeerreviewApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.beerreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerreviewApplication.class, args);
+	}
+
+}
---
+++ backend/beerreview/src/test/java/com/nttdata/beerreview/BeerreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.beerreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
