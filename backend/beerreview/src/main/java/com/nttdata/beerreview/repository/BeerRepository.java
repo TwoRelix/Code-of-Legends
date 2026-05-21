04af677f9ced1025baac19527d29b99cb30acea0
Gabriel Picazo <gabrielpicazo@MacBook-Air-de-Gabriel.local>
2026-05-21 12:06:19 +0200

    version3

--- backend/beerreview/src/main/java/com/nttdata/beerreview/repository/BeerRepository.java
+++ backend/beerreview/src/main/java/com/nttdata/beerreview/repository/BeerRepository.java
@@ -1,9 +1,9 @@
 package com.nttdata.beerreview.repository;
 
-import com.nttdata.beerreview.model.Brewery;
+import com.nttdata.beerreview.model.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BreweryRepository extends JpaRepository<Brewery, Long> {
+public interface BeerRepository extends JpaRepository<Beer, Long> {
 }
